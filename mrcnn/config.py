0445a321448a9a7ea7e3dda83ab3090c309ee32b
ARfan <www.775902719@qq.com>
2020-05-02 14:41:01 +0800

    target_attribute_ids = tf.cast(target_attribute_ids, 'float32')

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -191,12 +191,14 @@ class Config(object):
 
     # Loss weights for more precise optimization.
     # Can be used for R-CNN training setup.
+    # fan
     LOSS_WEIGHTS = {
         "rpn_class_loss": 1.,
         "rpn_bbox_loss": 1.,
         "mrcnn_class_loss": 1.,
         "mrcnn_bbox_loss": 1.,
-        "mrcnn_mask_loss": 1.
+        "mrcnn_mask_loss": 1.,
+        "mrcnn_attribute_loss": 1.
     }
 
     # Use RPN ROIs or externally generated ROIs for training
